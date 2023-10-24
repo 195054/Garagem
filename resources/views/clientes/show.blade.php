8b95bac4ef1d456195577eacc919d529a50b451f
a196366 <196366@lci-3-9.UPF>
2023-10-23 21:59:28 -0300

    Foi traduzido o breeze e finalizado CRUD dos clientes

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="author-details-layout">
-        <link rel="stylesheet" href="{{ asset('css/cliente.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/clientes.css') }}">
         <h1>Detalhes do Cliente</h1>
         <ul>
             <li><strong>ID:</strong> {{ $cliente->id }}</li>
@@ -9,6 +9,6 @@
             <li><strong>Telefone:</strong> {{  $cliente->telefone }}</li>
             <li><strong>Endereço:</strong> {{  $cliente->endereco }}</li>
         </ul>
-        <a href="{{ route('cliente.index') }}" class="btn btn-secondary">Voltar</a>
+        <a href="{{ route('clientes.index') }}" class="btn btn-secondary">Voltar</a>
     </div>
 </x-app-layout>
